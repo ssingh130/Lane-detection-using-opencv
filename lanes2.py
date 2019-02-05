3e12a55b32fae3c259b89b5a233be8a30942d0c7
ssingh130 <42983491+ssingh130@users.noreply.github.com>
2019-02-05 09:05:55 -0500

    Add files via upload

---
+++ lanes2.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def make_coordinates(image, line_parameters):
+	slope,intercept = line_parameters
+	y1 = image.shape[0]
+	y2 = int(y1*(3/5))
+	x1 = int((y1-intercept)/slope)
+	x2 = int((y2-intercept)/slope)
+	#print(image.shape)
+	return np.array([x1,y1,x2,y2])
+
+def averaged_slope_intercept (image,lines):
+	left_fit = []
+	right_fit = []
+	for line in lines:
+		x1,y1,x2,y2 = line.reshape(4)
+		parameters = np.polyfit((x1,x2),(y1,y2),1)
+		slope = parameters[0]
+		intercept = parameters[1]
+		if slope < 0:
+			left_fit.append((slope,intercept))
+		else:
+			right_fit.append((slope,intercept))
+	left_fit_avg = np.average(left_fit,axis=0)
+	right_fit_avg = np.average(right_fit,axis=0)
+	left_line = make_coordinates(image,left_fit_avg)
+	right_line = make_coordinates(image,right_fit_avg)
+	print (parameters)
+
+def canny(image):
+
+	gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+	blur = cv2.GaussianBlur(gray, (5, 5),0)
+	canny = cv2.Canny(blur,80,400)
+	return canny
+def display_lines (image,lines):
+	line_image = np.zeros_like(image)
+	if lines is not None:
+		for line in lines:
+			x1,y1,x2,y2 = line.reshape(4)
+			cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+			print (line)
+	return line_image
+
+def region_of_interest(image):
+	height = image.shape[0]
+	polygons = np.array([
+	[(170,height),(810,height),(500,250)]
+	])
+	mask = np.zeros_like(image)
+	cv2.fillPoly(mask,polygons, 255)
+	masked_image = cv2.bitwise_and(image, mask)       # used to mask the image only to show the	region of interest
+	return masked_image
+
+image = cv2.imread('test_image3.jpg')
+lane_image = np.copy(image)
+canny = canny(lane_image)
+cropped_image = region_of_interest(canny)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180,100,np.array([]),minLineLength = 40, maxLineGap = 8)
+averaged_lines = averaged_slope_intercept(lane_image, lines)
+line_image = display_lines(lane_image,lines)
+combo_img = cv2.addWeighted(lane_image,0.8, line_image,1,1 )
+#plt.imshow (canny)
+#plt.show()
+cv2.imshow('result',combo_img)
+cv2.waitKey(0)
